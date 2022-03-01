86b2013a94e8f5f8faa34ec8d9ce6e4c5dca8249
Romain <rmns06@gmail.com>
2022-03-01 15:32:08 +0100

    transform object

--- three_js/main.js
+++ three_js/main.js
@@ -8,15 +8,65 @@ document.querySelector('#app').innerHTML = `
 //scene
 const scene = new THREE.Scene();
 
-//object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+//object------------------------------------------
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 //material
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
 //final object (mesh)
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+//methods to transform object
+
+// mesh.position.z = 0.7;
+// mesh.position.y = 0.6;
+// mesh.position.set(0.7, 0.6, 0);
+
+//scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 1;
+// mesh.scale.set(2, 1, 1);
+
+//rotation
+// mesh.rotateZ(10);
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.y = 10;
+// mesh.rotation.x = 10;
+// rotate without change axes
+// mesh.rotateX(Math.PI * 0.2 );
+// mesh.rotateY(Math.PI * 0.1 );
+
+//add mesh to the scene
+//scene.add(mesh);
+
+//groups----------------------------------------
+const group = new THREE.Group()
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube2.position.set(1.5,.3,1)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({color: 'green'})
+)
+cube3.position.set(.8,1.2,-1.6)
+
+group.add(cube1, cube2, cube3)
+
+scene.add(group)
+
+group.rotateZ(0.2)
+
+//add gizmo
+const axesHelper = new THREE.AxesHelper(1.5)
+scene.add(axesHelper)
 
 const sizes ={
   width: 800,
@@ -25,8 +75,12 @@ const sizes ={
 //camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 1;
 
 scene.add(camera);
+
+camera.lookAt(group.position)
 
 //renderer to draw into canvas api
 const canvas = document.querySelector('.webgl')
