2f7b91894b8595968c9cadeb2519eb08b76927a1
Romain <rmns06@gmail.com>
2022-03-03 15:31:59 +0100

    feature/adding controls on camera and damping

--- three_js/main.js
+++ three_js/main.js
@@ -1,12 +1,30 @@
 import './style.css'
 import * as THREE from 'three';
 import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello three js!</h1>
   <h2>Part 1</h2>
   `
+const canvas = document.querySelector('.webgl')
+const sizes ={
+  width: 800,
+  height: 600
+}
+
+/**
+ * cursor
+ */
+const cursor = {
+  x: 0,
+  y:0
+}
+window.addEventListener('mousemove', (e) => {
+ cursor.x = e.clientX / sizes.width - 0.5
+ cursor.y = -(e.clientY / sizes.height - 0.5)
+})
 //scene
 const scene = new THREE.Scene();
 
@@ -14,7 +32,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 //material
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0x66C3FF });
 
 //final object (mesh)
 const mesh = new THREE.Mesh(geometry, material);
@@ -42,27 +60,36 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 
-
 //add gizmo
 const axesHelper = new THREE.AxesHelper(1.5)
 scene.add(axesHelper)
 
-const sizes ={
-  width: 800,
-  height: 600
-}
-//camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+/** 
+ *camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100);
+// const aspectRatio =  sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1* aspectRatio, 1, -1)
 camera.position.z = 3;
-camera.position.y = 1;
-camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.x = 2;
 
 scene.add(camera);
-
 // camera.lookAt(group.position)
 
+/**
+ * Controls camera
+ */
+ const controls = new OrbitControls(camera, canvas)
+// change the defaut view
+//  controls.target.y = 1
+//  controls.update()
+
+//add damping for smooth controls
+controls.enableDamping = true
+
+
 //renderer to draw into canvas api
-const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
@@ -78,11 +105,28 @@ const clock = new THREE.Clock()
 const tick = () => {
 
 const elapsedTime = clock.getElapsedTime()
-mesh.rotation.y =  elapsedTime
-camera.lookAt(mesh.position)
+// mesh.rotation.y =  elapsedTime
 
+/**
+ * Update Camera manually
+ */
+// to see the mesh
+// camera.position.x = cursor.x * 2 
+// camera.position.y = cursor.y * 2 
+
+//to see the mesh and behind
+// camera.position.x = Math.cos(cursor.x * Math.PI * 2) * 3 
+// camera.position.z = Math.sin(cursor.x * Math.PI * 2) * 3
+// camera.position.y = cursor.y*5
+// camera.lookAt(mesh.position)
+
+/**
+ * Update Camera with built in fnction OrbitControls
+ */
+controls.update()
+
+//Renderer
 renderer.render(scene, camera)
-
 requestAnimationFrame(tick)
 }
 tick()
